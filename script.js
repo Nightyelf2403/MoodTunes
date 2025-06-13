8d1774ac9c1e9f116440da19f6b9556172df6b6c
Lalith aditya Chunduri <chun8135@vandals.uidaho.edu>
2025-06-13 10:52:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const playlists = {
-  happy: "Try this: 🎶 [Happy Vibes Playlist](https://open.spotify.com/playlist/37i9dQZF1DXdPec7aLTmlC)",
-  sad: "Try this: 😔 [Sad Songs Playlist](https://open.spotify.com/playlist/37i9dQZF1DX7qK8ma5wgG1)",
-  energetic: "Try this: ⚡ [Workout Bangers](https://open.spotify.com/playlist/37i9dQZF1DX70RN3TfWWJh)",
-  chill: "Try this: 🧘 [Lo-Fi Chill Beats](https://open.spotify.com/playlist/37i9dQZF1DX4WYpdgoIcn6)"
+  happy: `😊 You're feeling happy! Try this: <a href="https://open.spotify.com/playlist/37i9dQZF1DXdPec7aLTmlC" target="_blank">Happy Vibes Playlist</a>`,
+  sad: `😢 Need a moment? Here's a <a href="https://open.spotify.com/playlist/37i9dQZF1DX7qK8ma5wgG1" target="_blank">Sad Songs Playlist</a>`,
+  energetic: `⚡ Let’s move! Check this out: <a href="https://open.spotify.com/playlist/37i9dQZF1DX70RN3TfWWJh" target="_blank">Workout Bangers</a>`,
+  chill: `🧘 Unwind here: <a href="https://open.spotify.com/playlist/37i9dQZF1DX4WYpdgoIcn6" target="_blank">Lo-Fi Chill Beats</a>`,
+  romantic: `💖 Feel the love: <a href="https://open.spotify.com/playlist/37i9dQZF1DX50QitC6Oqtn" target="_blank">Romantic Hits</a>`,
+  focus: `🧠 Stay sharp: <a href="https://open.spotify.com/playlist/37i9dQZF1DX3PFzdbtx1Us" target="_blank">Focus Flow Playlist</a>`
 };
 
 function suggest(mood) {
